a7f7841e046a69fc656b532961ebaeba6e0ced76
youssef-attai <youssefgalalnazem@gmail.com>
2022-12-14 21:59:26 +0200

    Create and test a game instance

--- main.cpp
+++ main.cpp
@@ -1,10 +1,17 @@
-int main() {
-/*
-    while (game is running) {
-        handle any user input
-        update all objects e.g. positions, etc.
-        render changes to the display
+#include "Game.h"
+
+Game *game = nullptr;
+
+int main(int argc, const char *argv[]) {
+    game = new Game();
+    game->init("My game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 300, 300);
+
+    while (game->running()) {
+        game->handleEvent();
+        game->update();
+        game->render();
     }
-*/
+    game->clean();
+    
     return 0;
 }
