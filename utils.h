9e86cdcd8ba61144185c7acaaa578395977ee44c
youssef-attai <youssefgalalnazem@gmail.com>
2022-12-15 00:56:59 +0200

    Add comments

--- utils.h
+++ utils.h
@@ -7,8 +7,10 @@ inline const char *asset(const char *_filename) {
     const char *assetsPath = "../assets/";
     const char *extension = ".png";
     char *filename = new char[std::strlen(assetsPath) + strlen(_filename) + strlen(extension) + 1];
+//    Prefix the asset name with the assets directory path
     strcpy(filename, assetsPath);
     strcat(filename, _filename);
+//    Append .png at the end of the filepath 
     strcat(filename, extension);
     return filename;
 }
